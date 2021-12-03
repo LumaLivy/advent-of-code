cbcd214d81375417cb4ce16f087caea6134922c2
LumaLivy <7350336+LumaLivy@users.noreply.github.com>
2021-12-03 16:50:36 -0500

    Add boilerplate file to speed things up a bit

---
+++ boilerplate.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import fs from "fs";
+import process from "process";
+import { getArgs, colorize } from "../shared/utils.js";
+
+const argv = getArgs(process.argv.slice(2), "00");
+
+const verbose = argv.v;
+const file = argv.f;
+
+// Main function to calculate and display part one
+const partOne = (data, verbose) => {
+  console.log("Todo");
+};
+
+// Main function to calculate and display part two
+const partTwo = (data, verbose) => {
+  console.log("Todo");
+};
+
+// Read the input data
+const processData = (data) =>
+  data
+    .trim()
+    .split("\n")
+    .map((n) => {});
+
+const main = () => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) return console.log(err);
+    const cleaned = processData(data);
+    if (verbose) console.log("---\nBeginning calcuation of part one:\n---");
+    const res1 = partOne(cleaned, verbose);
+    if (verbose) console.log("---\nBeginning calcuation of part two:\n---");
+    const res2 = partTwo(cleaned, verbose);
+
+    if (verbose) console.log("---\nResults:\n---");
+    console.log("Part One:", res1);
+    console.log("Part Two:", res2);
+  });
+};
+
+if (!argv.h) main();
