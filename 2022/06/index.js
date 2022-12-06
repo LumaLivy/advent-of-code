fbcb4ab815528581a684df8f4909e6fe84a2166e
LumaLivy <7350336+LumaLivy@users.noreply.github.com>
2022-12-06 12:08:41 -0500

    2022 Day 06

---
+++ 2022/06/index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import fs from "fs";
+import process from "process";
+import { getArgs, colorize } from "../../shared/utils.js";
+
+const argv = getArgs(process.argv.slice(2), "2022", "06");
+
+const verbose = argv.v;
+const file = argv.f;
+
+// Main function to calculate and display part one
+const partOne = (data) => {
+  const cleaned = processData(data)[0];
+  return (
+    1 +
+    cleaned.findIndex((s, si) => {
+      if (si < 3) return;
+      const set = [...new Set([...cleaned.slice(si - 3, si + 1)])];
+      return set.length === 4;
+    })
+  );
+};
+
+// Main function to calculate and display part two
+const partTwo = (data) => {
+  const cleaned = processData(data)[0];
+  return (
+    1 +
+    cleaned.findIndex((s, si) => {
+      if (si < 13) return;
+      const set = [...new Set([...cleaned.slice(si - 13, si + 1)])];
+      return set.length === 14;
+    })
+  );
+};
+
+// Read the input data
+const processData = (data) =>
+  data
+    .trim()
+    .split("\n")
+    .map((n) => n.split(""));
+
+const main = () => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) return console.log(err);
+    if (verbose) console.log("---\nBeginning calcuation of part one:\n---");
+    const res1 = partOne(data);
+    if (verbose) console.log("---\nBeginning calcuation of part two:\n---");
+    const res2 = partTwo(data);
+
+    if (verbose) console.log("---\nResults:\n---");
+    console.log("Part One:", res1);
+    console.log("Part Two:", res2);
+  });
+};
+
+if (!argv.h) main();
